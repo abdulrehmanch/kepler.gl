e5a13e609c650c4e193b4850cd53926d123c3b2f
abdulrehman <abdulrehman.ch@live.com>
2025-07-08 15:13:10 +0500

    Update login modal to use localhost API base URL for development

--- examples/demo-app/src/components/login-modal/index.tsx
+++ examples/demo-app/src/components/login-modal/index.tsx
@@ -55,7 +55,8 @@ const formContainerStyle = {
   marginTop: '20px'
 };
 
-const baseUrl = 'https://gridmaps.geosoftsolution.com'; // Replace with your actual base URL
+const baseUrl = 'http://localhost:8000'; // Replace with your actual base URL
+// const baseUrl = 'https://gridmaps.geosoftsolution.com';
 // Real API login endpoint function
 const loginEndpoint = async (
   username: string,
